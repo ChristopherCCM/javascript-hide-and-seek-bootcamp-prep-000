4f816274bcf670214bad9b6c9258430f61f24f37
Christopher <christopherccm@msn.com>
2018-03-31 15:23:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,8 @@ function getFirstSelector(selector) {
   const firstElement = document.querySelector(selector)
   return firstElement
 }
+
+function nestedTarget() {
+  const = targetElement = document.querySelector(#nested.target)
+  return targetElement
+}
