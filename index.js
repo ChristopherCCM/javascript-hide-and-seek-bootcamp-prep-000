8fef61fb949d34c14f1dc5dfad6c9778b8a48012
Christopher <christopherccm@msn.com>
2018-03-31 15:35:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function getFirstSelector(selector) {
 }
 
 function nestedTarget() {
-  //const targetElement = document.querySelector("#nested")
   const targetElement = document.getElementById('nested').querySelector('div.target')
+  //const targetElement = document.querySelector("#nested")     Also correctly selects 'nested'
   return targetElement
 }
